22fa4084674a912ab84181b6eb6622ada3aa2af9
소영 <sso_0022@naver.com>
2021-01-25 23:58:33 +0900

    [210125]

---
+++ 프로그래머스/신규 아이디 추천.playground/Contents.swift
@@ -0,0 +1,50 @@
+// 2021.01.25 MON
+// swift 코테 연습
+// 프로그래머스
+// Level 1
+// 신규 아이디 추천
+
+import UIKit
+
+var myID: String = "=.="
+
+//1차
+myID = myID.lowercased()
+
+//2차
+var newID: String = ""
+for i in myID {
+    if i.isLetter || i.isNumber || i == "-" || i == "_" || i == "." {
+        newID.append(i)
+    }
+}
+
+//3차
+while newID.contains("..") {
+    newID = newID.replacingOccurrences(of: "..", with: ".")
+}
+
+//4차
+while newID.hasPrefix(".") {
+    newID.remove(at: newID.startIndex)
+}
+
+//5차
+if newID == "" {
+    newID = "a"
+}
+
+//6차
+if newID.count >= 16 {
+    let index = newID.index(newID.startIndex, offsetBy: 15)
+    newID = String(newID[newID.startIndex..<index])
+}
+
+//7차
+if newID.count <= 2 {
+    while newID.count != 3 {
+        newID = newID + String(newID.last!)
+    }
+}
+
+print(newID)
