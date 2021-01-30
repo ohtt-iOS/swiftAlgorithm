7c2dde796f059d5397cf8779aab75949181a4bfc
소영 <sso_0022@naver.com>
2021-01-30 23:01:39 +0900

    [210130]

---
+++ 프로그래머스/순위 검색.playground/Contents.swift
@@ -0,0 +1,16 @@
+// 2021.01.30 SAT
+// swift 코테 연습
+// 프로그래머스
+// Level 2
+// 순위 검색
+
+
+import UIKit
+
+var info = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
+var query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
+
+var result: [Int] = []
+
+// 푸는중
+
